00a21086cd7d47a4eeafa07ba7bdb52c40096dda
jimmirbn <jimmirbn@gmail.com>
2023-02-14 12:58:46 +0100

    add experimental worker and add proxy redirect netlify

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head/>
       <body>
+        <noscript>
+          <iframe
+            title="GTM"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T8BJCPZ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
