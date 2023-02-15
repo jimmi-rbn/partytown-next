602f9afb3826e6c73416459e2ddb23fc0b17b656
jimmirbn <jimmirbn@gmail.com>
2023-02-15 12:19:23 +0100

    change gtm url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,9 +18,10 @@ export default function Home() {
           dangerouslySetInnerHTML={{
             __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='/gtm.js';
-            })(window,document,'script','dataLayer','GTM-T8BJCPZ')
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'/gtm.js';f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T8BJCPZ')
           `,
           }}
         />
