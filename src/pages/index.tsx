0e008b5b87c8700e323159cb48a5db14aa8049bf
jimmirbn <jimmirbn@gmail.com>
2023-02-15 12:32:49 +0100

    add cookie back

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,27 +13,26 @@ export default function Home() {
 
         <Partytown debug={true} forward={["dataLayer.push"]} />
         <script
+          type="text/partytown"
+          id="CookieConsent"
+          src="/cookieinformation.js"
+          data-culture="EN"
+        ></script>
+        <script
           id="gtm"
           type="text/partytown"
           dangerouslySetInnerHTML={{
             __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'/gtm.js';f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-T8BJCPZ')
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            '/gtm.js';f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T8BJCPZ')
           `,
           }}
         />
       </Head>
 
-      {/* <Script
-        strategy="worker"
-        id="CookieConsent"
-        src="/cookieinformation.js"
-        data-culture="EN"
-        type="text/javascript"
-      ></Script> */}
       <main>Partytown next</main>
     </>
   );
